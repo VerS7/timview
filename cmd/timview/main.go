767bb0954aa0a2a3b347a6a397a1816187789413
VerS7 <thegoversus@gmail.com>
2025-11-24 12:44:09 +0300

    refactoring

--- cmd/timview/main.go
+++ cmd/timview/main.go
@@ -11,9 +11,8 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 
-	"timview/internal/resize"
-
 	"timview/internal/platform"
+	"timview/internal/resize"
 	"timview/internal/util"
 )
 
@@ -43,8 +42,80 @@ var (
 	nowarn  = flag.Bool("nowarn", false, "disables WARN messages")
 )
 
-func main() {
+func RenderImage(img image.Image) string {
+	max := img.Bounds().Max
 
+	var finalImg strings.Builder
+	finalImg.Grow(max.Y / 2)
+
+	var row strings.Builder
+	row.Grow(max.X * len(FG+BG+SEGMENT))
+
+	for y := 0; y < max.Y; y += 2 {
+		row.Reset()
+
+		for x := 0; x < max.X; x += 1 {
+			ur, ug, ub, _ := img.At(x, y).RGBA()
+			lr, lg, lb, _ := img.At(x, y+1).RGBA()
+
+			var fragment string
+
+			if y+1 >= max.Y {
+				fragment = fmt.Sprintf(
+					FG+SEGMENT,
+					// Normalize to 0-255
+					ur>>8,
+					ug>>8,
+					ub>>8,
+				)
+			} else {
+				fragment = fmt.Sprintf(
+					FG+BG+SEGMENT,
+					// Normalize to 0-255
+					ur>>8,
+					ug>>8,
+					ub>>8,
+					lr>>8,
+					lg>>8,
+					lb>>8,
+				)
+			}
+			row.WriteString(fragment)
+		}
+		finalImg.WriteString(row.String())
+		finalImg.WriteString(RESET)
+		finalImg.WriteString(NEWLINE)
+	}
+
+	finalImg.WriteString(RESET)
+	return finalImg.String()
+}
+
+func ProcessImage(imagepath string, targetWidth int, scale float64) (image.Image, error) {
+	// Open image file
+	file, err := os.Open(imagepath)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file %s", imagepath)
+	}
+	defer file.Close()
+
+	// Decode image
+	img, format, err := image.Decode(file)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode image with format %s", format)
+	}
+
+	scale = util.Clamp(scale, 0.1, 1.0)
+
+	newWidth := math.Round(float64(targetWidth) * scale)
+
+	smpls := *samples
+	smpls = util.Clamp(smpls, 2, 16)
+
+	return resize.Resize(uint(newWidth), 0, img, resize.MitchellnetravaliInterp, smpls), nil
+}
+
+func main() {
 	out := os.Stdout
 	outFd := int(out.Fd())
 
@@ -58,10 +129,11 @@ func main() {
 	disableWarnings := *nowarn
 
 	// Enable colored output (platform specific)
-	if enabled := platform.EnableColoredOutput(outFd); !enabled {
+	if colorEnabled := platform.EnableColoredOutput(outFd); !colorEnabled {
 		panic("ERROR: colored output not supported")
 	}
 
+	// Check if FD is terminal
 	if !platform.IsTerminal(outFd) {
 		if !disableWarnings {
 			fmt.Fprintln(out, "WARN: not in terminal")
@@ -77,71 +149,16 @@ func main() {
 		width = 100
 	}
 
-	filepath := flag.Arg(0)
-	if filepath == "" {
+	imagepath := flag.Arg(0)
+	if imagepath == "" {
 		panic("ERROR: image path is empty")
 	}
 
-	// Open image file
-	file, err := os.Open(filepath)
+	img, err := ProcessImage(imagepath, width, *ratio)
 	if err != nil {
-		panic(fmt.Errorf("ERROR: could not open file %s", filepath))
+		panic(fmt.Errorf("ERROR: %s", err))
 	}
-	defer file.Close()
-
-	// Decode image
-	img, format, err := image.Decode(file)
-	if err != nil {
-		panic(fmt.Errorf("ERROR: could not decode image with format %s", format))
-	}
-
-	aspectRatio := *ratio
-	aspectRatio = util.Clamp(aspectRatio, 0.1, 1.0)
-
-	newWidth := math.Round(float64(width) * aspectRatio)
-
-	smpls := *samples
-	smpls = util.Clamp(smpls, 2, 16)
-
-	newImg := resize.Resize(uint(newWidth), 0, img, resize.MitchellnetravaliInterp, smpls)
-
-	max := newImg.Bounds().Max
 
 	fmt.Fprint(out, CLEAR)
-
-	var finalImage strings.Builder
-	finalImage.Grow(max.Y / 2)
-
-	var row strings.Builder
-	row.Grow(max.X * len(FG+BG+SEGMENT))
-
-	for y := 0; y < max.Y; y += 2 {
-		row.Reset()
-
-		for x := 0; x < max.X; x += 1 {
-			ur, ug, ub, _ := newImg.At(x, y).RGBA()
-			lr, lg, lb, _ := newImg.At(x, y+1).RGBA()
-
-			fragment := fmt.Sprintf(
-				FG+BG+SEGMENT,
-				// Normalize to 0-255
-				ur>>8,
-				ug>>8,
-				ub>>8,
-				lr>>8,
-				lg>>8,
-				lb>>8,
-			)
-
-			row.WriteString(fragment)
-		}
-		finalImage.WriteString(row.String())
-		finalImage.WriteString(RESET)
-		finalImage.WriteString(NEWLINE)
-	}
-
-	finalImage.WriteString(RESET)
-	finalImage.WriteString(NEWLINE)
-
-	fmt.Fprint(out, finalImage.String())
+	fmt.Fprint(out, RenderImage(img))
 }
